9bfa8c05de254dd7f83185922e162244bc3e6f22
dikshya-2000 <106343560+dikshya-2000@users.noreply.github.com>
2022-09-03 20:52:44 +0530

    Delete PHP directory

--- PHP/homeaction.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('connection.php');
-echo "<script>alert('Please login to continue!')</script>";
-echo "<script>window.location.href='signin.php'</script>";
-?>
--- PHP/likeaction1.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<script>alert('Thank you for liking our website!')</script>";
-echo "<script>window.location.href='index.php'</script>";
-?>
