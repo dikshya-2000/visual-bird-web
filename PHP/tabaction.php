93738448a42565f0233b93cac45c6843151a295a
dikshya-2000 <106343560+dikshya-2000@users.noreply.github.com>
2022-09-03 22:50:38 +0530

    Delete PHP directory

--- PHP/tabaction.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('connection.php');
-echo "<script>alert('Please sign in/sign up to access the webpage!')</script>";
-echo "<script>window.location.href='signin.php'</script>";
-
-?>
--- PHP/tabaction3.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('connection.php');
-echo "<script>alert('Please login to shop!')</script>";
-echo "<script>window.location.href='signin.php'</script>";
-
-?>
